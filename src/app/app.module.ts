9a678bd8a98258f45c594086a16363a57c101021
Seung Lee <jkobb510@yahoo.com>
2023-11-01 14:36:45 -0400

    new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExperienceComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule
