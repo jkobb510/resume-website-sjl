014dbe2e7b8bb4983ab84428a4997109fc5db7d1
jkobb510 <jkobb510@yahoo.com>
2024-01-09 21:34:06 -0500

    style changes

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.css']
 })
 export class ExperienceComponent {
-
 }
